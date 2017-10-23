ab1e947264617cdbb436a59a0cd328cdc0a6edc1
FurZzZ <tomswi91@gmail.com>
2017-10-23 15:46:44 +0200

    First commit

---
+++ lib/func.py
@@ -0,0 +1,39 @@
+'''
+	Description: Project to analyse http headers of your app.
+	Email: -
+	Version: 0.1
+'''
+
+import sys
+import urllib2
+
+
+
+
+def validate_user_input():
+	if len(sys.argv) < 2:
+		return False
+	else:
+		if check_if_url_is_valid(sys.argv[1]) === True:
+			return True
+		else:
+			return False
+
+def display_message_in_color(message, status=WHITE):
+	# displays messages in defined colors (white, red, yellow, orange, blue)
+	pass
+
+def check_if_url_is_valid(url):
+	# returns true if url is valid
+	pass
+
+
+def analyze_headers(url):
+	# analyzes the http headers (https://www.owasp.org/index.php/OWASP_Secure_Headers_Project#tab=Headers)
+	pass
+
+
+def run():
+	if validate_user_input() === True:
+		url = sys.argv[1]
+		analyze_headers(url)
---
+++ sechead.py
@@ -0,0 +1,3 @@
+from lib.func import *
+
+run()
